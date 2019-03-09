b94a29e2b362ab2d8c916a625c7daf668537d0df
Nicolas Deslandes <ndeslandes@live.fr>
2019-03-09 18:40:36 +0000

    Add close button

--- components/StoryModal.js
+++ components/StoryModal.js
@@ -3,21 +3,21 @@ import { TouchableHighlight, Image, Text, SafeAreaView, View, StyleSheet } from 
 import type { Story } from './StoryModel';
 
 type StoryModalProps = {
-    story: Story,
-    onClose: () => mixed
+  story: Story,
+  onClose: () => mixed
 }
 
 export default class StoryModal extends Component<StoryModalProps> {
   render() {
-    const {story, onClose} = this.props;
+    const { story, onClose } = this.props;
 
     return (
       <View style={styles.container}>
         <Image source={story.source} style={styles.image} />
         <SafeAreaView>
-            <TouchableHighlight style={styles.closeButton} onPress={onClose}>
-                <Text>X</Text>
-            </TouchableHighlight>
+          <TouchableHighlight style={styles.closeButton} onPress={onClose}>
+            <Text style={styles.buttonText}>x</Text>
+          </TouchableHighlight>
         </SafeAreaView>
       </View>
     )
@@ -25,17 +25,30 @@ export default class StoryModal extends Component<StoryModalProps> {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,        
-        backgroundColor: "#fff",
-        flex: 1
-    },
-    image: {
-        ...StyleSheet.absoluteFillObject,
-    },
-    closeButton: {
-        marginTop: 30,
-        margin: 20,
-        alignSelf: 'flex-end'
-      }
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "#fff",
+    flex: 1
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  closeButton: {
+    marginTop: 30,
+    margin: 20,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#555',
+    width: 30,
+    height: 30,
+    borderRadius: 30,
+    opacity: 0.5,
+  },
+  buttonText: {
+    borderWidth: 0,
+    borderColor: 'red',
+    marginLeft: 1,
+    marginTop: -2,
+    fontSize: 20
+  },
 })
